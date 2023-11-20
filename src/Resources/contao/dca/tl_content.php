4cd651e942ba9ef3b1082733a9e190def19f4380
laemmi <laemmi@spacerabbit.de>
2023-11-20 11:59:10 +0100

    set ctsimagetextalign default NULL

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -46,6 +46,6 @@ $GLOBALS['TL_DCA'][$strName]['fields']['ctsimagetextalign'] = array
     'inputType'               => 'select',
     'options'                 => array('center','left','right'),
     'eval'                    => array('tl_class' => 'w50', 'alwaysSave' => true),
-    'sql'                     => "text NOT NULL"
+    'sql'                     => "text NULL"
 );
 
